bd0fd675b3134080d846cbe7df63ab29db214c61
Amir01020 <rodjabovamir@gmail.com>
2024-01-18 20:16:19 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,123 @@
+// let numbers = 123 //Числовой тип данных, включая целые числа и числа с плавающей запятой.
+// let str = "name" //Строковый тип данных для текстовой информации
+// let Booleans = true // false Логический тип данных, принимающий значения true или false
+// let obj3 ={
+//     name:"1",
+//     price:"",
+//     price2:"",
+// }
+// let obj1 ={
+//     name:"2",
+//     price:""
+// }
+// let obj2 ={
+//     name:"3",
+//     price:""
+// }
+// console.log(obj2);
+
+// let arr1 = [
+//     {
+//         name:"tel",
+//         price:"",
+//         img:"",
+//     },{
+//         name:"num",
+//         price:"",
+//         img:"",
+//     },{
+//         name:"pk",
+//         price:"",
+//         img:"",
+//     },{
+//         name:"mp3",
+//         price:"",
+//         img:"",
+//     },
+// ]
+// console.log(arr1[0].name);
+
+// let myItem = 1
+// let item //Undefined null
+
+
+
+// let item1 = 4
+
+// let form = item1**2
+
+// console.log(form);
+
+
+// let item2 = 5
+// let item3 = item1 % item2
+
+
+
+// console.log(item3);
+
+
+
+
+
+
+
+
+// let a = 12
+// let b = 66
+// let e = 32
+// let r = 35
+// let t = 433
+// let c = a/b
+// let maxNumber = Math.max(a,b,e,r,t);
+// let minNumber = Math.min(a,b,e,r,t);
+// let arr = ["amir","min","alex","maxNumber","Math","max"]
+
+// let random = Math.random()*6
+
+
+// let num = 20.134141414
+// console.log(Math.ceil(num));
+
+// let item = Math.ceil(Math.random()*6)
+// console.log(item);
+
+
+
+
+
+
+// let j = +prompt()
+// let a = +prompt()
+
+// let j ="2"
+// let a = "1"
+// j=+j
+// a=+a
+
+
+
+
+// console.log(j + a);
+
+
+let a = +prompt()
+let b = +prompt()
+let  c = a**2+2*a*b+b**2
+
+
+console.log(c);
+
+
+
+// let btn = document.querySelector("button")
+// let text = document.querySelector("h1")
+
+// btn.onclick=()=>{
+//     console.log(text.innerText);
+
+// }
+
+
+
+
